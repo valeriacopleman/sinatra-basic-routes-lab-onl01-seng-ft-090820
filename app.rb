f6bc761c1affc013c8164cee71dd8df4135811a2
valeriacopleman <valeriacopleman@gmail.com>
2020-10-16 10:30:13 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+      end
+
+    get '/name' do 
+        "My name is Valeria Copleman."
+    end
+
+    get '/hometown' do 
+        "My hometown is Temecula."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Demons."
+    end
+
 end
